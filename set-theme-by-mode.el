0966e55ea7030aac4443f57994665ab37a62cf47
Yi Wang <yiwang@servian.com>
2022-07-18 13:43:29 +1000

    finish theme code for vterm.

--- set-theme-by-mode.el
+++ set-theme-by-mode.el
@@ -1,13 +1,19 @@
-;; adapted from http://xahlee.info/emacs/emacs/emacs_set_theme_on_mode.html
+;; use regex because sometimes we need more than one vterm frame/window!
+(setq special-display-regexps '("\\*vterm\\**"))
 
+;; adapted from http://xahlee.info/emacs/emacs/emacs_set_theme_on_mode.html
 (defun set-vterm-theme ()
   "Set background color or theme depending on major mode."
-  (message "setting theme.")
-  (if (eq major-mode 'vterm-mode)
+  (if (and
+       ;; interestingly eq is enough to test equality here
+       (eq major-mode 'vterm-mode) 
+       ;; I'm happy with dracula theme so don't run this
+       ;; if dracula's enabled
+       (not (equal custom-enabled-themes '(dracula))))
       (progn 
-        (set-background-color "black")
-        (set-foreground-color "white"))))
+        (message "setting basic theme for vterm.")
+        (set-background-color "#282a36")
+        (set-foreground-color "#f8f8f2"))))
 
 (add-hook 'vterm-mode-hook 'set-vterm-theme)
 
-(setq special-display-buffer-names '("*vterm*"))
