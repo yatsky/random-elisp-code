d77a41a094c9aca0517eb9031c4bbc3b46cc3ab9
Yaoni <y.wang7@uq.net.au>
2023-01-10 19:35:16 +1100

    add yaoni/get-code-location to easily get filename + line number of code

--- yaoni-get-buffer-name.el
+++ yaoni-get-buffer-name.el
@@ -4,4 +4,9 @@
   (interactive)
   (kill-new (buffer-name)))
 
+(defun yaoni/get-code-location ()
+  "Gets the current file name (via buffer name) and the line number."
+  (interactive)
+  (kill-new (concat (buffer-name) (string-replace "Line" ":" (what-line)))))
+
 (provide 'yaoni-get-buffer-name)
