9d65a81e4f1dc43a5786047c3ef8ffc632b7e911
Yaoni <y.wang7@uq.net.au>
2023-01-15 21:07:31 +1100

    update yaoni/get-code-location

--- yaoni-get-buffer-name.el
+++ yaoni-get-buffer-name.el
@@ -5,8 +5,15 @@
   (kill-new (buffer-name)))
 
 (defun yaoni/get-code-location ()
-  "Gets the current file name (via buffer name) and the line number."
+  "Gets the repo name, file name (via buffer name) and the line number."
   (interactive)
-  (kill-new (concat (buffer-name) (string-replace "Line" ":" (what-line)))))
+  (let ((root-dir
+         ;; second (2) to last
+         (car (last (split-string (vc-root-dir) "/") 2))))
+    (kill-new  (concat (replace-regexp-in-string
+                        (concat ".*" root-dir)
+                        root-dir
+                        (buffer-file-name))
+                       (string-replace "Line" ":" (what-line))))))
 
 (provide 'yaoni-get-buffer-name)
